de997b5c00e271d6b5ac97342d204632a00684ee
kmpalombo91 <kmpalombo91@gmail.com>
2025-01-30 15:52:45 -0600

    commit files for user service

--- src/main/java/com/kpalombo/user_service/config/SecurityConfig.java
+++ src/main/java/com/kpalombo/user_service/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.kpalombo.user_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,9 +19,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                // ...
-                .csrf((csrf) -> csrf.disable());
+        http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/login/oauth2/code/spotify").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(Customizer.withDefaults()).csrf().disable();
         return http.build();
     }
 }
