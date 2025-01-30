117b337ea2f20546a842db568e2b643643fcf104
kmpalombo91 <kmpalombo91@gmail.com>
2025-01-30 16:11:22 -0600

    commit files for user service

--- src/main/java/com/kpalombo/user_service/controller/UserController.java
+++ src/main/java/com/kpalombo/user_service/controller/UserController.java
@@ -11,8 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 @CollectionRecordRepository(path = "/users")
@@ -43,6 +46,33 @@ public class UserController extends CollectionController<User, UUID> {
         record.setPassword(passwordEncoder.encode(record.getPassword()));
         User updatedRecord = repository.save(record);
         response.setResponse(new ResponseEntity<>(updatedRecord, HttpStatus.OK));
+        return response;
+    }
+
+    @GetMapping("/spotify/callback")
+    public Response<User> handleSpotifyCallback(OAuth2AuthenticationToken authentication) {
+        Response<User> response = new Response<>();
+        Map<String, Object> attributes = authentication.getPrincipal().getAttributes();
+        String spotifyId = (String) attributes.get("id");
+        String email = (String) attributes.get("email");
+        String username = (String) attributes.get("display_name");
+
+        Optional<User> existingUser = ((UserRepository)repository).findByEmail(email);
+        User user;
+        if (existingUser.isPresent()){
+            user = existingUser.get();
+            if (user.getSpotifyId() == null){
+                user.setSpotifyId(spotifyId);
+            }
+        }
+        else{
+            user = new User();
+            user.setEmail(email);
+            user.setUsername(username);
+            user.setSpotifyId(spotifyId);
+            repository.save(user);
+        }
+        response.setResponse(new ResponseEntity<>(user, HttpStatus.OK));
         return response;
     }
 }
--- src/main/java/com/kpalombo/user_service/repository/UserRepository.java
+++ src/main/java/com/kpalombo/user_service/repository/UserRepository.java
@@ -4,6 +4,9 @@ import com.kpalombo.music_mgmt_collection_library.interfaces.BaseRepository;
 import com.kpalombo.user_service.model.User;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends BaseRepository<User> {
+    Optional<User> findByEmail(String email);
 }
