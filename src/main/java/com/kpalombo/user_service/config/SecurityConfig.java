f3ad4118afd55f607e0daedf4257c97ac0d7f0f6
kmpalombo91 <kmpalombo91@gmail.com>
2025-02-03 14:23:54 -0600

    commit files for user service

--- src/main/java/com/kpalombo/user_service/config/SecurityConfig.java
+++ src/main/java/com/kpalombo/user_service/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/login/oauth2/code/spotify").permitAll()
+                        .requestMatchers("/users/login/oauth2/code/spotify").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2Login(Customizer.withDefaults()).csrf().disable();
